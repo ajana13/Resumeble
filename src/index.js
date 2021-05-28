3fa5df51b9757f2e37a9af64bfae85d8b60d927f
NamPhan56 <namp@umass.edu>
2021-05-28 15:59:05 -0400

    removed server mod, moved index.js to src dir

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-"" 
+
+const http = require('http')
+const server = http.createServer();
+const path = require('path')
+
+const express = require('express')
+const app = express()
+app.use(express.static('public'))
+
+app.listen(3000,() => {
+  console.log("Appp listening on port 3000")
+})
