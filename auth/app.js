2b1462c72f332fc295f1a27de74ddad60f26c4da
HerlinRijo <70146991+HerlinRijo@users.noreply.github.com>
2022-03-30 19:26:16 -0400

    Revert "Resolved auth problems"
    This reverts commit 7d240a3a6aa10129b765f16edccd922f1d6169f0.

--- auth/app.js
+++ auth/app.js
@@ -1,30 +1,30 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const passport = require("passport");
+const express = require('express');
+const mongoose = require('mongoose');
+const passport = require('passport');
 
-const path = require("path");
-const multer = require("multer");
-require("dotenv").config();
+const path = require('path');
+const multer = require('multer');
+require('dotenv').config({ path: path.resolve(__dirname, './config/.env') });
 
 // eslint-disable-next-line no-unused-vars
-const auth = require("./routes/auth");
-const { useJwtStrategy } = require("./util/passport/index");
+const auth = require('./routes/auth');
+const { useJwtStrategy } = require('./util/passport/index');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 // DB Config
 const mongoURI = process.env.NODE_ENV
-  ? process.env.MONGO_URI
-  : process.env.DEV_MONGO_URI;
+    ? process.env.MONGO_URI
+    : process.env.DEV_MONGO_URI;
 
 // Connect to MongoDB
 mongoose
-  .connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  // eslint-disable-next-line no-console
-  .then(() => console.log("MongoDB successfully connected"))
-  // eslint-disable-next-line no-console
-  .catch((err) => console.log(err));
+    .connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    // eslint-disable-next-line no-console
+    .then(() => console.log('MongoDB successfully connected'))
+    // eslint-disable-next-line no-console
+    .catch((err) => console.log(err));
 
 // Bodyparser middleware
 app.use(express.json());
@@ -36,25 +36,25 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.use(
-  require("express-session")({
-    secret: "build",
-    resave: false,
-    saveUninitialized: false,
-  })
+    require('express-session')({
+        secret: 'build',
+        resave: false,
+        saveUninitialized: false,
+    })
 );
 
 // Multer Middleware
 const multerMid = multer({
-  storage: multer.memoryStorage(),
-  limits: {
-    fileSize: process.env.MAX_FILE_SIZE,
-  },
+    storage: multer.memoryStorage(),
+    limits: {
+        fileSize: process.env.MAX_FILE_SIZE,
+    },
 });
-app.use(multerMid.single("file"));
-app.disable("x-powered-by");
+app.use(multerMid.single('file'));
+app.disable('x-powered-by');
 
 // Routes
-app.use("/auth", auth);
+app.use('/auth', auth);
 // app.use(express.static('client/build'));
 
 // Serve static assets (build folder) if in production
@@ -65,6 +65,6 @@ app.get('*', (req, res) => {
 */
 
 app.listen(port, () => {
-  // eslint-disable-next-line no-console
-  console.log(`BUILD UMass server running on http://localhost:${port}`);
+    // eslint-disable-next-line no-console
+    console.log(`BUILD UMass server running on http://localhost:${port}`);
 });
--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -9,19 +9,19 @@ import SkillsCard from "./models/skillsCard";
 import ExperienceCard from "./models/experienceCard";
 import EducationCard from "./models/educationCard";
 import ProjectsCard from "./models/projectsCard";
-//import {useState} from 'react'
-//import { Worker } from '@react-pdf-viewer/core';
+import {useState} from 'react'
+import { Worker } from '@react-pdf-viewer/core';
 // Import the main Viewer component
-//import { Viewer } from '@react-pdf-viewer/core';
+import { Viewer } from '@react-pdf-viewer/core';
 // Import the styles
-//import '@react-pdf-viewer/core/lib/styles/index.css';
+import '@react-pdf-viewer/core/lib/styles/index.css';
 // default layout plugin
-//import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
+import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
 // Import styles of default layout plugin
-//import '@react-pdf-viewer/default-layout/lib/styles/index.css';
+import '@react-pdf-viewer/default-layout/lib/styles/index.css';
 
 const Profile = ({ auth }) => {
-  /*
+
   // creating new plugin instance
   const defaultLayoutPluginInstance = defaultLayoutPlugin();
   
@@ -54,7 +54,7 @@ const Profile = ({ auth }) => {
       console.log('please select a PDF');
     }
   }
-*/
+
   useEffect(() => {
     // Init Tabs Materialize JS
     let fixed = document.querySelectorAll(".fixed-action-btn");
@@ -104,52 +104,70 @@ const Profile = ({ auth }) => {
         <div style={{ marginTop: "-20rem" }} className="row">
           <div className="col s12">
             <div className="card horizontal">
-              <div className="card-image">
-                <img src={profile} />
+            <div className="card-image">
+                <img src={profile}  />
               </div>
               <div className="card-stacked">
                 <div className="card-content">
                   <div className="row">
-                    <div className="col s12">
-                      <h4>Herlin Rijo</h4>
-                    </div>
-                    <div className="col s12">
-                      <span class="card-title">Software Engineer </span>
-                    </div>
-                    <div className="col s12">
-                      <h6 className="grey-text">Phone:</h6>
-                    </div>
-                    <div className="col s12">
-                      <p>774-823-0167</p>
-                    </div>
 
-                    <div className="col s12">
-                      <h6 className="grey-text">Email:</h6>
-                    </div>
-                    <div className="col s12">
-                      <p>hrijo@umass.edu</p>
-                    </div>
 
-                    <div className="col s12">
-                      <h6 className="grey-text">Adress:</h6>
-                    </div>
-                    <div className="col s12">
-                      <p>151 CommonWealth Ave, Amherst MA</p>
-                    </div>
+                  <div className="col s12">
+                  <h4>Herlin Rijo</h4>
+                     </div>
+                  <div className="col s12">
+                  <span class="card-title">Software Engineer </span>
+                     </div>
+                  <div className="col s12">
+                  <h6 className="grey-text">
+                    Phone:
+                  </h6>
+                  </div>
+                  <div className="col s12">
+                  <p>
+                    774-823-0167
+                  </p>
+                  </div>
+                  
+                  <div className="col s12">
+                  <h6 className="grey-text">
+                    Email:
+                  </h6>
+                  </div>
+                  <div className="col s12">
+                  <p>
+                    hrijo@umass.edu
+                  </p>
+                  </div>
 
-                    <div className="col s12">
-                      <h6 className="grey-text">Website:</h6>
-                    </div>
-                    <div className="col s12">
-                      <p>www.hrijo.com</p>
-                    </div>
+                  <div className="col s12">
+                  <h6 className="grey-text">
+                    Adress:
+                  </h6>
+                  </div>
+                  <div className="col s12">
+                  <p>
+                    151 CommonWealth Ave, Amherst MA
+                  </p>
+                  </div>
+
+                  <div className="col s12">
+                  <h6 className="grey-text">
+                    Website:
+                  </h6>
+                  </div>
+                  <div className="col s12">
+                  <p>
+                    www.hrijo.com
+                  </p>
+                  </div>
+
                   </div>
                 </div>
                 <div className="card-action">
-                  <a href="#">Edit Information</a>
-                  <a className="modal-trigger " href="#modal2">
-                    Upload Resume
-                  </a>
+                  <a  href="#">Edit Information</a>
+                  <a  className="modal-trigger "
+                href="#modal2">Upload Resume</a>
                 </div>
               </div>
             </div>
@@ -251,24 +269,73 @@ const Profile = ({ auth }) => {
       </div>
 
       <div id="modal2" className="modal">
-        <div className="modal-content">
-          <div>
-            <h4>Upload Resume</h4>
-            <p>
-              This action is unreversable, click cancel to keep skill or delete
-              to continue with removal
-            </p>
-          </div>
-        </div>
-        <div className="modal-footer">
-          <a href="#!" class="modal-close btn-flat">
-            Cancel
-          </a>
-          <a href="#!" class="modal-close green white-text btn">
-            Save
-          </a>
-        </div>
+    <div className="modal-content">
+      <div>
+      <h4>Upload Resume</h4>
+      <p>This action is unreversable, click cancel to keep skill or delete to continue with removal</p>
+
+  <form action="#">
+    <div className="file-field input-field">
+      <div className="btn">
+        <span>File</span>
+        <input style={{borderBottom:"1px solid rgb(0, 0, 0)"}}type="file"/>
       </div>
+      <div className="file-path-wrapper">
+        <input className="file-path validate" type="text"/>
+      </div>
+    </div>
+  </form>
+
+
+
+  <div className="container">
+
+{/* Upload PDF */}
+<form>
+
+  <label><h5>Upload PDF</h5></label>
+  <br></br>
+
+  <input type='file' className="form-control"
+  onChange={handleFile}></input>
+
+  {/* we will display error message in case user select some file
+  other than pdf */}
+  {pdfError&&<span className='text-danger'>{pdfError}</span>}
+
+</form>
+
+{/* View PDF */}
+<h5>View PDF</h5>
+<div className="viewer">
+
+  {/* render this if we have a pdf file */}
+  {pdfFile&&(
+    <Worker workerUrl="https://unpkg.com/pdfjs-dist@2.12.313/build/pdf.worker.min.js">
+      <Viewer fileUrl={pdfFile}
+      plugins={[defaultLayoutPluginInstance]}></Viewer>
+    </Worker>
+  )}
+
+  {/* render this if we have pdfFile state null   */}
+  {!pdfFile&&<>No file is selected yet</>}
+
+</div>
+
+</div>
+
+
+
+
+
+      </div>
+    </div>
+    <div className="modal-footer">
+      <a href="#!" class="modal-close btn-flat">Cancel</a>
+      <a href="#!" class="modal-close green white-text btn">Save</a>
+    </div>
+  </div>
+
     </div>
   );
 };
